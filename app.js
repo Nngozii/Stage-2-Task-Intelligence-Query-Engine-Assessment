16ae43842f3ac0677c6d2e4657b0b83f46b21df6
Ngozi <hexwhyzed01@gmail.com>
2026-04-23 10:19:05 +0100

    feat: add get all profiles logic / endpoint

--- app.js
+++ app.js
@@ -8,7 +8,28 @@ const pool = require("./db");
 const port = 9200;
 const app = express();
 
+// Get All Profiles Endpoint
+app.get("/api/profiles", async (req, res) => {
+  const {name, age, gender, } = req.query
+  try {
+    const result = await pool.query("SELECT * FROM profiles");
+    const profiles = result.rows;
 
+    if (!profiles) {
+      return res.status(404).json({ status: "error", message: "Not found" });
+    }
+
+    res.status(200).json({
+      status: "success",
+      page: 1,
+      limit: 10,
+      total: 2026,
+      data: profiles
+    });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 app.listen(port, () => {
   console.log("Server listen on port", port);
