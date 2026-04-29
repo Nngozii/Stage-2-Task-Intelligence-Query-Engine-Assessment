6774bfb2b9a3e897f965bb44e68fde9a8e371ce4
Ngozi <hexwhyzed01@gmail.com>
2026-04-29 14:00:53 +0100

    feat: add the auth router api

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ const { uuidv7 } = require("uuidv7");
 
 const pool = require("./db");
 const profileRouter = require("./routers/profiles.route");
+const authRouter = require("./routers/auth.route");
 
 const port = 9200;
 const app = express();
 
-
-
 app.use("/api", profileRouter);
+app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Insighta Labs+");
 });
-
 
 app.listen(port, () => {
   console.log("Server listen on port", port);
